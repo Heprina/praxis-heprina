bdf6776d343572332ea4a7935a01e1e4aeacfe33
heprina <heprina.inf18@student.unu-jogja.ac.id>
2021-10-05 13:30:11 +0700

    harikedua

---
+++ 01-02/harikedua.py
@@ -0,0 +1,32 @@
+#tipe data integer
+data_integer = 25
+print(data_integer)
+
+#tipe data float
+data_float= 30.5
+print("data :", data_float )
+print(type( data_float) )
+
+#tipe data string
+data_string = "hari kedua belajar tipe data"
+print(data_string)
+print(type( data_string) )
+
+#tipe data bolean
+data_bool = True
+print(data_bool)
+print(type( data_bool) )
+
+#tipe data list
+thislist1 = ("pulpen")
+thislist2 =(" buku")
+thislist3 = ("pensil")
+print (thislist1)
+print (type (thislist2))
+
+#tipe data dictonary
+biodata = ("nama : heprina husriana ",
+ "nim : 181111111",
+ "prodi : informatika")
+print (biodata)
+print (type(biodata))
