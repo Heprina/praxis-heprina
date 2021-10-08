a2d9761c04873d22727c6228ea43ba381863c9f7
heprina <heprina.inf18@student.unu-jogja.ac.id>
2021-10-08 17:16:08 +0700

    pertemuan ke-5 minggu pertama

--- 01-04/mytugas/tugas/urls.py
+++ 01-04/mytugas/tugas/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hapus/<id>/',views.hapus),
+    path('detail/<id>/', views.detail),
+    path('edit/<id>/', views.edit),
+    
 ]
-#bikin tabel
-
--- 01-04/mytugas/tugas/views.py
+++ 01-04/mytugas/tugas/views.py
@@ -1,5 +1,5 @@
 from django.db.models.fields.json import DataContains
-from django.shortcuts import  render
+from django.shortcuts import  redirect, render
 
 from . import models
 #create your views here.
@@ -7,12 +7,35 @@ from . import models
 
 def index(request):
     if request.POST : 
-        # data = request.POST ['name']
-        # print (data)
+        data = request.POST ['name']
+        print (data)
         #input data ke database
-        models.tugas.objects.create(name = request.POST ['name'])
+        models.tugas.objects.create(name = data)
         #nampilin datanya
     data2 = models.tugas.objects.all()
+    print(data2)
 
     return render (request, 'index.html', {'isi' :data2})
-   
+def hapus (request, id):
+    models.tugas.objects.filter(id = id).delete()
+    return redirect('/')
+
+
+def detail (request, id):
+    data = models.tugas.objects.filter(id = id).first()
+    print(data)
+    return render (request, "detail.html", {
+        "detail.data": data
+    })  
+
+def edit (request, id):
+    if request.POST:
+        input = request.POST["name"]
+        print(input)
+        models.tugas.objects.filter(id = id).update(name = input)
+        return redirect('/')
+
+    data = models.tugas.objects.filter(id = id).first()
+    return render (request, "edit.html", {
+        "detailData": data,
+    })  
